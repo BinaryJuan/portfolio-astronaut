90b5554fd2c9ab66c5d4f20c6231dae3db0e1a71
Dante Terranova <93538499+BinaryJuan@users.noreply.github.com>
2022-10-19 01:00:43 -0300

    Responsive fix

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -7,11 +7,11 @@ const Presentation = () => {
                 <div className='fElement fImg'><img className='pImage' src='media/satellite.png' alt='satellite' /></div>
                 <div className='fElement'>
                     <p className='pText'>
-                        My name is Dante Terranova, i am a programmer who is passionate for the IT field,
+                        My name is Dante Terranova, I am a programmer who is passionate for the IT field,
                         more specifically into developing desktop applications, websites and databases.
                         I consider myself a skillful individual at problem solving and would be interested
                         in new challenges.
-                        My native language is Spanish, although i have an advanced level of English which
+                        My native language is Spanish, although i have an advanced level of English (C1 Advanced) which
                         makes me able to communicate efficiently and adapt to different scenarios
                     </p>
                 </div>
--- src/components/ScollTopAnimation/ScrollTopAnimation.js
+++ src/components/ScollTopAnimation/ScrollTopAnimation.js
@@ -6,6 +6,12 @@ const ScrollTopAnimation = () => {
             <a href='/#'>
                 <img src='media/purple-rocket.png' alt='scroll top' />
             </a>
+            <div class="containerFlame">
+                <div class="red flame"></div>
+                <div class="orange flame"></div>
+                <div class="yellow flame"></div>
+                <div class="white flame"></div>
+            </div>
         </div>
     )
 }
