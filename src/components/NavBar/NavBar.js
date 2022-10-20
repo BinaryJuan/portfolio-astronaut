01cdd198f100206ad699eda65a525a605ac16061
Dante Terranova <93538499+BinaryJuan@users.noreply.github.com>
2022-10-20 15:08:24 -0300

    Responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,28 +1,6 @@
 import './NavBar.css'
 import { Link } from 'react-scroll'
 
-const getScrollPercent = () => {
-    let h = document.documentElement, 
-        b = document.body,
-        st = 'scrollTop',
-        sh = 'scrollHeight';
-    return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
-}
-
-const background = () => {
-    let scrollPercent = getScrollPercent()
-    if (scrollPercent > 30) {
-        document.getElementById('myNavbar').style.background = 'rgba(0, 0, 0, 0.5)'
-        document.getElementById('myNavbar').style.borderRadius = '0 0 0 10px'
-    } else {
-        document.getElementById('myNavbar').style.background = 'transparent'
-    }
-}
-
-window.addEventListener('scroll', () => {
-    document.getElementById('myNavbar') ? background() : window.removeEventListener('scroll', () => {})
-})
-
 const NavBar = () => {
     return (
         <nav className='nContainer' id='myNavbar'>
